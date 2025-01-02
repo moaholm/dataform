139048dca1428fa6902a438e777f8cffe4243041
moaholm <mkeholmberg@gmail.com>
2025-01-02 08:11:37 +0100

    kladdkod deluxe

--- kladdkod/products_table.sql
+++ kladdkod/products_table.sql
@@ -9,12 +9,12 @@ INNER JOIN `data-evolution-moa.raw_wwi.stockItemStockGroup` AS products
 ON category.stockGroupID = products.stockGroupID
 )
     SELECT
-    s1.stockItemID AS productID,
+    s1.productID,
     productInformation.stockItemName,
     productInformation.unitPrice,
     productInformation.recommendedRetailPrice,
-    s1.stockGroupID AS categoryID,
-    s1.stockGroupName AS categoryName
+    s1.categoryID,
+    s1.categoryName
 FROM `data-evolution-moa.raw_wwi.stockItem` AS productInformation
 INNER JOIN Step1 AS s1
 ON s1.productID = productInformation.stockItemID
